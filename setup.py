b246e55ed79c055df4878f4f34ddd1f0fb5ed437
Daniel Leppänen <daniel.leppanen@gmail.com>
2020-01-20 16:08:05 +0100

    Update setup.py with package information

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="halpa-schema-validator",
+    name="schema-validator-halpa",
     version="0.0.1",
     author="Daniel Leppänen",
     author_email="daniel.leppanen@gmail.com",
     description="A utility to validate JSON schemas with jsonschema with simplejson",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/halpdesk/example-pkg",
+    url="https://github.com/halpdesk/python-halpa-schema-validator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
